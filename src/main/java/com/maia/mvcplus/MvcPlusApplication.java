4e9d98c1acc559d66549f0c0f72d6c276fb8a2ca
Dowglas Maia <dowglasmaia@live.com>
2018-10-31 14:36:55 -0300

    Projeto Criado

---
+++ src/main/java/com/maia/mvcplus/MvcPlusApplication.java
@@ -0,0 +1,12 @@
+package com.maia.mvcplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPlusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maia/mvcplus/MvcPlusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maia.mvcplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvcPlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
