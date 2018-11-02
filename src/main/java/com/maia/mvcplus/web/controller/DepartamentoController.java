3c5b91c09cfc628662fad77732c4dd67180e8250
Dowglas Maia <dowglasmaia@live.com>
2018-11-02 19:45:21 -0300

    Implementando o Salvar do Controller e Ligando com o Front da Pagina

--- src/main/java/com/maia/mvcplus/web/controller/DepartamentoController.java
+++ src/main/java/com/maia/mvcplus/web/controller/DepartamentoController.java
@@ -1,22 +1,36 @@
 package com.maia.mvcplus.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.maia.mvcplus.domain.Departamento;
+import com.maia.mvcplus.interfaces.DepartamentoServices;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	private DepartamentoServices servives;
+
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "departamento/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar() {
 		return "departamento/lista";
 	}
-	
+
+	// Salvar
+	@PostMapping("/salvar")
+	public String Salvar(Departamento departamento) {
+		servives.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
 
 }
